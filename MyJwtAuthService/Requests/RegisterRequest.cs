9fed8a76f8bf056c95fdcdb719a1d6af13b777bd
ser45g <sergey1pes@gmail.com>
2026-05-21 19:43:57 +0300

    Took away data attributes from RegisterRequest since we use FluentValidation

--- MyJwtAuthService/Requests/RegisterRequest.cs
+++ MyJwtAuthService/Requests/RegisterRequest.cs
@@ -4,17 +4,12 @@ namespace MyJwtAuthService.Requests
 {
     public class RegisterRequest
     {
-        [Required]
-        [EmailAddress]
         public required string Email { get; set; }
 
-        [Required]
         public required string Username { get; set; }
 
-        [Required]
         public  required string Password { get; set; }
 
-        [Required]
         public required string ConfirmPassword { get; set; }
     }
 }
