bb2be1d6ad2eb20f7935c56286dbbf7f754a739c
ser45g <sergey1pes@gmail.com>
2026-07-14 03:01:50 +0300

    Added client project

---
+++ Webhooks.Client/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.MapPost("webhooks", () =>
+{
+
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.Run();
